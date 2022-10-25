979a9e3b9e8b8b9208ce032959abc3fa1defac6d
HANJAEWOOK <khai84837521@gmail.com>
2022-10-25 23:39:27 +0900

    인증 클래스 구현체 생성

---
+++ src/main/java/com/example/shop_toy/infrastructure/member/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.example.shop_toy.infrastructure.member;
+
+import com.example.shop_toy.domain.member.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return memberRepository.findById(username)
+                .map(this::createUserDetails)
+                .orElseThrow(() -> new UsernameNotFoundException("회원을 찾을 수 없습니다."));
+    }
+
+    // 해당하는 User 의 데이터가 존재한다면 UserDetails 객체로 만들어서 리턴
+    private UserDetails createUserDetails(Member member) {
+        return new User(member.getMemberId(), member.getMemberPassword(), member.getAuthorities());
+    }
+}
